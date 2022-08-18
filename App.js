f840be75e68fe7b8e0137b2cd0aa46afc6a4d04b
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-08-18 11:57:46 +0200

    Ajout champ texte, bouton et liste lien

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput defaultValue="Nouveau lien..." style={styles.inputLien} />
+        <Button title="Ajouter un lien" />
+      </View>
+      <View style={styles.lienContainer}>
+        <Text>Liste des liens:</Text>
+      </View>
     </View>
   );
 }
@@ -11,8 +17,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#8D77D8",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  styleTexte: {
+    color: "red",
+  },
+  inputLien: {
+    borderColor: "limegreen",
+    borderWidth: 1,
+    padding: 1,
+    margin: 5,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 4,
+    backgroundColor: "#BAABEE",
+  },
+  lienContainer: {
+    backgroundColor: "#A396D0",
+    padding: 4,
   },
 });
